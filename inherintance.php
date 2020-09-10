545e7ac62cc267961a6f3a4c8e87853774c85ef9
AlifaNovelia <69961570+AlifaNovelia@users.noreply.github.com>
2020-09-10 13:56:48 +0700

    Add files via upload

--- inherintance.php
+++ inherintance.php
@@ -19,5 +19,27 @@ class produk{
    
    public function cetakinfo(){
        $str="{$this->namabarang},{$this->getcetak()}";
+       return $str;
    }
 }
+ class laptop extends produk{
+     public function cetakinfo(){
+         $str="laptop : {$this->namabarang}, {$this->getcetak()} | ukuran layar: {$this->ukuranlayar}";
+         return $str;
+     }
+ }
+
+ class flashdisk extends produk{
+     public function cetakinfo(){
+         $str="Aksesoris: {$this->namabarang}, {$this->getcetak()}|kapasitas: {$this->kapasitas}";
+     }
+ }
+ $produk1 =new laptop("idepad 310", "lenovo", 7000000,"14 inci","-");
+ $produk2 = new flashdisk("Flashdisk", "Sandisk", 100000,"-","8 GB");
+
+ echo $produk1->cetakinfo();
+ echo "<br>";
+ echo $produk2->cetakinfo();
+ echo "<br>";
+  
+ 
